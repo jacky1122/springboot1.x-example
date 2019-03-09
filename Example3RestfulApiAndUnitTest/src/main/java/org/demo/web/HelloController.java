2e6923a314a023c59d1dd3fa8a61dde4b0b21b71
run-zheng <run.zheng548@gmail.com>
2019-03-10 00:01:00 +0800

    add example3  restfulapi and unit test

---
+++ Example3RestfulApiAndUnitTest/src/main/java/org/demo/web/HelloController.java
@@ -0,0 +1,14 @@
+package org.demo.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller 
+public class HelloController {
+	@RequestMapping("/")
+	public String index(ModelMap map ) {
+		map.addAttribute("host", "https://www.baidu.com");
+		return "index";
+	}
+}
