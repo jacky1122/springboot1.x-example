69f8683bc54703933eadbe2568e16372c78e2b57
run-zheng <run.zheng548@gmail.com>
2019-03-10 00:40:28 +0800

    add example 4 swagger2

---
+++ Example4Swagger2/src/main/java/org/demo/Example4Swagger2.java
@@ -0,0 +1,13 @@
+package org.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Example4Swagger2 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Example4Swagger2.class, args);
+	}
+
+}
